786f828f50768ebe46d2c6b886e8364e3e0b0829
Guadalupe Atim <guadiiatim1309@gmail.com>
2023-07-10 18:53:46 -0300

    mostrando precios de monedas y corrigiendo errores

--- Program.cs
+++ Program.cs
@@ -27,8 +27,14 @@ internal class Program
                     using (StreamReader objReader = new StreamReader(strReader))
                     {
                         string responseBody = objReader.ReadToEnd();
-                        Bpi moneda = JsonSerializer.Deserialize<Bpi>(responseBody);
+                        Moneda mon = JsonSerializer.Deserialize<Moneda>(responseBody);
 
+                        Console.WriteLine("-------PRECIOS-------");
+                        Console.WriteLine("USD: $" + mon.bpi.USD.rate_float);
+                        Console.WriteLine("EUR: $" + mon.bpi.EUR.rate_float);
+                        Console.WriteLine("GBP: $" + mon.bpi.GBP.rate_float);
+
+                        //elegir moneda
                         Console.WriteLine("Ingrese una moneda: 1-USD, 2-EUR, 3-GBP");
                         int opcion;
                         bool control = int.TryParse(Console.ReadLine(), out opcion);
@@ -38,25 +44,25 @@ internal class Program
                             switch (opcion)
                             {
                                 case 1:
-                                    Console.WriteLine("Code: " + moneda.USD.code);
-                                    Console.WriteLine("Symbol: " + moneda.USD.symbol);
-                                    Console.WriteLine("Rate: " + moneda.USD.rate);
-                                    Console.WriteLine("Description: " + moneda.USD.description);
-                                    Console.WriteLine("Rate Float: " + moneda.USD.rate_float);                                
+                                    Console.WriteLine("Code: " + mon.bpi.USD.code);
+                                    Console.WriteLine("Symbol: " + mon.bpi.USD.symbol);
+                                    Console.WriteLine("Rate: $" + mon.bpi.USD.rate);
+                                    Console.WriteLine("Description: " + mon.bpi.USD.description);
+                                    Console.WriteLine("Rate Float: $" + mon.bpi.USD.rate_float);                                
                                     break;
                                 case 2:
-                                    Console.WriteLine("Code: " + moneda.EUR.code);
-                                    Console.WriteLine("Symbol: " + moneda.EUR.symbol);
-                                    Console.WriteLine("Rate: " + moneda.EUR.rate);
-                                    Console.WriteLine("Description: " + moneda.EUR.description);
-                                    Console.WriteLine("Rate Floar: " + moneda.EUR.rate_float);
+                                    Console.WriteLine("Code: " + mon.bpi.EUR.code);
+                                    Console.WriteLine("Symbol: " + mon.bpi.EUR.symbol);
+                                    Console.WriteLine("Rate: $" + mon.bpi.EUR.rate);
+                                    Console.WriteLine("Description: " + mon.bpi.EUR.description);
+                                    Console.WriteLine("Rate Float: $" + mon.bpi.EUR.rate_float);
                                     break;
                                 case 3:
-                                    Console.WriteLine("Code: " + moneda.GBP.code);
-                                    Console.WriteLine("Symbol: " + moneda.GBP.symbol);
-                                    Console.WriteLine("Rate: " + moneda.GBP.rate);
-                                    Console.WriteLine("Description: " + moneda.GBP.description);
-                                    Console.WriteLine("Rate Floar: " + moneda.GBP.rate_float);
+                                    Console.WriteLine("Code: " + mon.bpi.GBP.code);
+                                    Console.WriteLine("Symbol: " + mon.bpi.GBP.symbol);
+                                    Console.WriteLine("Rate: $" + mon.bpi.GBP.rate);
+                                    Console.WriteLine("Description: " + mon.bpi.GBP.description);
+                                    Console.WriteLine("Rate Float: $" + mon.bpi.GBP.rate_float);
                                     break;
                                 default:
                                     Console.WriteLine("La moneda ingresada no se encuentra disponible");
